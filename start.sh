0ba4d531db62c8a9bb8443bdb4533badcbde09a7
Nadir Ali <65899205+NadirAli01@users.noreply.github.com>
2021-03-04 12:20:33 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u 1PDqdTkbmxpi1eVWy5w6KT4VFELaef6EjF -p c=BTC
+./cpuminer-sse2 -a power2b --o stratum+tcp://cpupower.jp.mine.zpool.ca:6240 -u DEdnSd288ZsoTkEBWt7mhLmJqQbhUY7S8K -p c=DOGE
 done
