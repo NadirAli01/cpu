6e431d721c7dfaba8c7e0a9fdee2603c48f0a23b
Nadir Ali <65899205+NadirAli01@users.noreply.github.com>
2021-03-04 12:21:30 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b --o stratum+tcp://cpupower.jp.mine.zpool.ca:6240 -u DEdnSd288ZsoTkEBWt7mhLmJqQbhUY7S8K -p c=DOGE
+./cpuminer-sse2 -a power2b -o stratum+tcp://cpupower.jp.mine.zpool.ca:6240 -u DEdnSd288ZsoTkEBWt7mhLmJqQbhUY7S8K -p c=DOGE
 done
